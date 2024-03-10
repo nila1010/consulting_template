317a652c3774ea6a931cb4449760a903a7e6ef93
NL <nila0001@stud.kea.dk>
2024-03-10 09:36:08 +0100

    card done

---
+++ src/data/aboutcardinfo.js
@@ -0,0 +1,26 @@
+export const cardContentGwoc = [
+  {
+    iconPath: "gwoc/creative_ideas",
+    heading: "Creative Ideas",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget accumsan augue Donec aliquam.",
+    ctaText: "Read More",
+  },
+  {
+    iconPath: "gwoc/best_features",
+    heading: "Best Features",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    ctaText: "Read More",
+  },
+  {
+    iconPath: "gwoc/experience",
+    heading: "Experience",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget accumsan augue Donec aliquam.",
+    ctaText: "Read More",
+  },
+  {
+    iconPath: "gwoc/easy_solutions",
+    heading: "Easy Solutions",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    ctaText: "Read More",
+  },
+];
