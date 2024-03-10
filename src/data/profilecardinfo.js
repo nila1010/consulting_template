4da11d7ed6404372532ec50fbdad5003a86d9d4b
NL <nila0001@stud.kea.dk>
2024-03-10 19:09:08 +0100

    mangler billeder

---
+++ src/data/profilecardinfo.js
@@ -0,0 +1,25 @@
+export const profileContent = [
+  {
+    imagePath: "/src/assets/sarah.png",
+    altText: "profile picture",
+    name: "Sarah Jasmine",
+    text: "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+    work: "CEO at Company",
+    bgColor: "var(--secondary-color-2)",
+    colorText: "var(--secondary-color-1)",
+  },
+  {
+    imagePath: "/src/assets/john.png",
+    altText: "profile picture",
+    name: "John Carter",
+    text: "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+    work: "Manager",
+  },
+  {
+    imagePath: "/src/assets/daisy.png",
+    altText: "profile picture",
+    name: "Daisy Stella",
+    text: "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+    work: "CEO at Company",
+  },
+];
