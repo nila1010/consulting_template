0fa8362926137a467ea43cad3bb9f9684db7d1e8
NL <nila0001@stud.kea.dk>
2024-03-11 16:04:08 +0100

    v4

---
+++ src/data/financialdata.js
@@ -0,0 +1,22 @@
+export const contentFinancial = [
+  {
+    iconPath: "financial_cardicon_1",
+    heading: "The Balance Sheet",
+    text: "The Balance Sheet is a summary of the assets and liabilities and equity of a business at a specific point of time.",
+  },
+  {
+    iconPath: "financial_cardicon_2",
+    heading: "Income Statement",
+    text: "The Income (Profit and Loss) Statement, commonly referred to as the P&L statement, summarizes the revenue and expenses for a specific time period (one month, one quarter, one year, etc.)",
+  },
+  {
+    iconPath: "financial_cardicon_3",
+    heading: "Financial Plan",
+    text: "Keep your information current and review the documents on a regular basis (monthly or more often if needed). Review them with key individuals within your company. ",
+  },
+  {
+    iconPath: "financial_cardicon_4",
+    heading: "Financial Ratios",
+    text: "Ratios are useful when comparing your company with the competition on financial performance and also when benchmarking the performance of your company.",
+  },
+];
