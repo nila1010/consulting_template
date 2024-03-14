27748202385b902dcbfb9ec20529196138adc22a
NL <nila0001@stud.kea.dk>
2024-03-14 18:42:16 +0100

    contact done

---
+++ src/data/profilecardinfo2.js
@@ -0,0 +1,23 @@
+export const profileContent2 = [
+  {
+    imagePath: "/src/assets/maya.png",
+    altText: "profile picture",
+    name: "Maya Elizabeth",
+    text: "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+    work: "CEO at Company",
+  },
+  {
+    imagePath: "/src/assets/ryan.png",
+    altText: "profile picture",
+    name: "Ryan Jackson",
+    text: "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+    work: "CEO at Company",
+  },
+  {
+    imagePath: "/src/assets/lilly.png",
+    altText: "profile picture",
+    name: "Lilly Victoria",
+    text: "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+    work: "CEO at Company",
+  },
+];
